e6ae2120f152b9f2a1f5bb2af0baa051005ac7d5
Arsyi Adlani <arsyi.adlani@gmail.com>
2023-12-27 14:26:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ for message in st.session_state.messages:
         with st.chat_message(name="user", avatar="🧑"):
             st.markdown(message["content"])
     elif message["role"]=="assistant":
-        with st.chat_message(name="assistant"):
+        with st.chat_message(name="assistant", avatar="https://cdn-icons-png.flaticon.com/512/167/167752.png"):
             st.markdown(message["content"])
 
 if prompt := st.chat_input("Tanya LSProf..."):
