6dfe8fb9ced4bd877ce24f04563d640ddac1fdf6
Arsyi Adlani <arsyi.adlani@gmail.com>
2023-12-27 15:54:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ st.set_page_config(page_title="LSProf Virtual AI Assistant",
                    initial_sidebar_state="auto", 
                    menu_items=None)
 
+st.image('https://drive.google.com/file/d/12oGJUQwv2IhXEtWiX6rIpCHfkp6kMf5g/view?usp=sharing', caption='Sunrise by the mountains')
 st.title("Tanya apa saja seputar LSP Astra dengan LSProf 👇🏻")
 
 template_message_1 = "Apa yang dimaksud dengan Lembaga Sertifikasi Profesi (LSP) Astra?"
