71e99367190a6ff184b1dd3cbdeae449d0503157
Arsyi Adlani <arsyi.adlani@gmail.com>
2023-12-27 15:06:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,34 +35,7 @@ def clear_session():
     requests.delete(API_URL_RAG)
     requests.delete(API_URL_SMALL_TALK_CHATBOT)
 
-st.set_page_config(page_title="LSProf Virtual AI Assistant", 
-                   page_icon=page_avatar, 
-                   layout="centered", 
-                   initial_sidebar_state="auto", 
-                   menu_items=None)
-
-st.title("Tanya apa saja seputar LSP Astra dengan LSProf 👇🏻")
-
-template_message_1 = "Apa itu LSP Astra?"
-template_message_2 = "Bagaimana cara mengikuti sertifikasi profesi di LSP Astra?"
-template_message_3 = "Apa saja syarat untuk mendaftar sertifikasi profesi di LSP Astra?"
-
-template_button_1 = st.button(template_message_1)
-template_button_2 = st.button(template_message_2)
-template_button_3 = st.button(template_message_3)
-
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    if message["role"]=="user":
-        with st.chat_message(name="user", avatar=human_avatar):
-            st.markdown(message["content"])
-    elif message["role"]=="assistant":
-        with st.chat_message(name="assistant", avatar=ai_avatar):
-            st.markdown(message["content"])
-
-if prompt := st.chat_input("Tanya LSProf..."):
+def ask_prompt(prompt):
     with st.chat_message(name="user", avatar=human_avatar):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -94,6 +67,89 @@ if prompt := st.chat_input("Tanya LSProf..."):
             message_placeholder.markdown(full_response)
         st.session_state.messages.append({"role": "assistant", "content": response["text"]})
 
+    
+
+st.set_page_config(page_title="LSProf Virtual AI Assistant", 
+                   page_icon=page_avatar, 
+                   layout="centered", 
+                   initial_sidebar_state="auto", 
+                   menu_items=None)
+
+st.title("Tanya apa saja seputar LSP Astra dengan LSProf 👇🏻")
+
+template_message_1 = "Apa itu LSP Astra?"
+template_message_2 = "Bagaimana cara mengikuti sertifikasi profesi di LSP Astra?"
+template_message_3 = "Apa saja syarat untuk mendaftar sertifikasi profesi di LSP Astra?"
+template_message_4 = "Apa saja skema sertifikasi profesi yang ada di LSP Astra?
+
+template_button_1 = st.button(template_message_1)
+template_button_2 = st.button(template_message_2)
+template_button_3 = st.button(template_message_3)
+template_button_4 = st.button(template_message_4)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    if message["role"]=="user":
+        with st.chat_message(name="user", avatar=human_avatar):
+            st.markdown(message["content"])
+    elif message["role"]=="assistant":
+        with st.chat_message(name="assistant", avatar=ai_avatar):
+            st.markdown(message["content"])
+
+if template_button_1:
+    prompt = template_message_1
+    ask_prompt(prompt=prompt)
+elif template_button_2:
+    prompt = template_message_2
+    ask_prompt(prompt=prompt)
+elif template_button_3:
+    prompt = template_message_3 
+    ask_prompt(prompt=prompt)
+elif template_button_4:
+    prompt = template_message_4
+    ask_prompt(prompt=prompt)
+else:
+    prompt = st.chat_input("Tanya LSProf...")
+    ask_prompt(prompt=prompt)
+
+# if prompt := st.chat_input("Tanya LSProf..."):
+    # with st.chat_message(name="user", avatar=human_avatar):
+    #     st.markdown(prompt)
+    # st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # # Check whether small talk or not
+    # small_talk_or_not = query_small_talk_classifier({"question": prompt})['text']
+    # if small_talk_or_not == "Yes":
+    #     response = query_small_talk_chatbot({"question": prompt})
+    #     with st.chat_message(name="assistant", avatar=ai_avatar):
+    #         assistant_response = response['text']
+    #         message_placeholder = st.empty()
+    #         full_response = ""
+    #         for chunk in assistant_response.split():
+    #             full_response += chunk + " "
+    #             time.sleep(0.09)
+    #             message_placeholder.markdown(full_response + "▌")
+    #         message_placeholder.markdown(full_response)
+    #     st.session_state.messages.append({"role": "assistant", "content": response["text"]})
+    # else:
+    #     response = query_rag({"question": prompt})
+    #     with st.chat_message(name="assistant", avatar=ai_avatar):
+    #         assistant_response = response['text']
+    #         message_placeholder = st.empty()
+    #         full_response = ""
+    #         for chunk in assistant_response.split():
+    #             full_response += chunk + " "
+    #             time.sleep(0.09)
+    #             message_placeholder.markdown(full_response + "▌")
+    #         message_placeholder.markdown(full_response)
+    #     st.session_state.messages.append({"role": "assistant", "content": response["text"]})
+
+
+
+
+    
 
     # # Check whether small talk or not
     # small_talk_or_not = query_small_talk_classifier({"question": prompt})['text']
